a369d2a350b21bf31cc7656fdb74b1702f895f1b
“VIKASVERMA1” <vikasverma@excellencetechnologies.in>
2022-05-20 14:17:08 +0530

    expresstask1

---
+++ userSchema.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+
+const userSchema=mongoose.Schema({
+    firstName:{
+        type:String,
+        require:true
+    },
+    lastName:{
+        type:String,
+        require:true
+    },
+    userName:{
+        type:String,
+        require:true,
+        lowercase: true,
+        trim: true
+    },
+    email_id:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String
+    },
+    confirmPassword:{
+        type:String
+    }
+})
+module.exports=Users=mongoose.model('userdata',userSchema);
